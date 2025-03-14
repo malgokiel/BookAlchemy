0bee7ca7b6bfcddaa180254624cdf95399ae32f4
malgorzata <libronika@gmail.com>
2025-03-14 16:48:59 +0100

    Classes Book and Author creates

---
+++ data_models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from pandas.core.interchange.from_dataframe import primitive_column_to_ndarray
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    birth_date = db.Column(db.String)
+    date_of_death = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Author: {self.name}, born {self.birth_date}, died {self.date_of_death}'
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String)
+    title = db.Column(db.String)
+    publication_year = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+
+    def __repr__(self):
+        return f'Book: {self.title}, ISBN {self.isbn}'
