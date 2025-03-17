b7d1a855dd3a7566046cfd8d9edf88b71f02a3a0
malgorzata <libronika@gmail.com>
2025-03-17 19:50:29 +0100

    delete and sort functionalities added

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ import helper
 
 QUERY_ALL_AUTHORS = "SELECT authors.id, authors.name FROM authors"
 QUERY_ALL_BOOKS = "SELECT books.*, authors.name FROM books JOIN authors ON books.author_id = authors.id"
+QUERY_SORTED_AUTHORS = "SELECT books.*, authors.name FROM books JOIN authors ON books.author_id=authors.id ORDER BY authors.name"
+QUERY_SORTED_TITLES = "SELECT books.*, authors.name FROM books JOIN authors ON books.author_id=authors.id ORDER BY books.title"
+QUERY_SORTED_YEARS = "SELECT books.*, authors.name FROM books JOIN authors ON books.author_id=authors.id ORDER BY books.publication_year DESC"
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/malgorzata/PycharmProjects/BookAlchemy/data/library.sqlite'
 db.init_app(app)
+
+
+engine = create_engine('sqlite:///data/library.sqlite')
 
 @app.route('/add_author', methods=['GET', 'POST'])
 def add_author():
@@ -46,9 +52,28 @@ def add_book():
     return render_template('add_book.html', authors=authors)
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     books = helper.get_all_results(QUERY_ALL_BOOKS)
+    if request.method == 'POST':
+        action = request.form.get('sort')
+        delete = request.form.get('delete')
+        if action:
+            if action == 'author':
+                books = helper.get_all_results(QUERY_SORTED_AUTHORS)
+            elif action == 'title':
+                books = helper.get_all_results(QUERY_SORTED_TITLES)
+            elif action == 'year':
+                books = helper.get_all_results(QUERY_SORTED_YEARS)
+        elif delete:
+            book_id = delete
+            book_to_delete = db.session.get(Book, book_id)
+            db.session.delete(book_to_delete)
+            db.session.commit()
+            books = helper.get_all_results(QUERY_ALL_BOOKS)
+
+        return render_template('home.html', books=books)
+
     return render_template('home.html', books=books)
 
 with app.app_context():
