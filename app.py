9b725f7765cf3436b495003b1959e6613ca4b5c1
malgorzata <libronika@gmail.com>
2025-03-17 16:23:19 +0100

    css added, home templated created and styled

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from sqlalchemy import insert, text, create_engine
 from data_models import db, Author, Book
 import helper
 
+QUERY_ALL_AUTHORS = "SELECT authors.id, authors.name FROM authors"
+QUERY_ALL_BOOKS = "SELECT books.*, authors.name FROM books JOIN authors ON books.author_id = authors.id"
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/malgorzata/PycharmProjects/BookAlchemy/data/library.sqlite'
 db.init_app(app)
@@ -36,15 +39,21 @@ def add_book():
                             author_id=request.form.get('author'))
             db.session.add(new_book)
             db.session.commit()
-            authors = helper.get_all_authors()
+            authors = helper.get_all_results(QUERY_ALL_AUTHORS)
             return render_template('add_book.html', message="Book added.", authors=authors)
     else:
-        authors = helper.get_all_authors()
+        authors = helper.get_all_results(QUERY_ALL_AUTHORS)
     return render_template('add_book.html', authors=authors)
 
+
+@app.route('/home', methods=['GET'])
+def home():
+    books = helper.get_all_results(QUERY_ALL_BOOKS)
+    return render_template('home.html', books=books)
+
+with app.app_context():
+  db.create_all()
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
 
-# with app.app_context():
-#   db.create_all()
